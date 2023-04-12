05f50d690a4ca2acc26b9dc536ad60227e7a8780
Nazia Shehnaz Joynab <ncoder76@gmail.com>
2023-04-12 11:39:03 +0600

    hackerrank added

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ from flask import Flask, render_template
 import cses_scraper as cses_sc
 import loj_scraper as loj_sc
 import leetcode_scraper as lc_sc
+import hr_scraper as hr_sc
 
 app = Flask(__name__)
 
-oj = ['cses', 'loj', 'leetcode']
-solved =[cses_sc.cses,loj_sc.loj, lc_sc.leetcode]
-usernames =[cses_sc.cses_username, loj_sc.loj_username, lc_sc.lc_user]
-urls = [cses_sc.cses_url, loj_sc.loj_url, 'https://leetcode.com/'+lc_sc.lc_user]
+oj = ['cses', 'loj', 'leetcode','hackerrank']
+solved =[cses_sc.cses,loj_sc.loj, lc_sc.leetcode, hr_sc.total_count]
+usernames =[cses_sc.cses_username, loj_sc.loj_username, lc_sc.lc_user, hr_sc.payload['username']]
+urls = [cses_sc.cses_url, 
+        loj_sc.loj_url, 
+        'https://leetcode.com/'+lc_sc.lc_user,
+        'https://www.hackerrank.com/'+hr_sc.payload['username']
+        ]
 
 
 @app.route('/')
